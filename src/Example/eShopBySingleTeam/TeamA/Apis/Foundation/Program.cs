46b4826edce421a3367eef999bd3cbb4c45bde8c
VincentH.NET <dev@vincenth.net>
2022-11-01 09:20:46 +0100

    add example eShop by single team - sln with empty Apis

---
+++ src/Example/eShopBySingleTeam/TeamA/Apis/Foundation/Program.cs
@@ -0,0 +1,26 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app
+        .UseSwagger()
+        .UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+sealed partial class Program { } // Fix CA1852
