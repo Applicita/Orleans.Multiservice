ec6c907b95413d34df3e0f20190e8649fe7d4c58
VincentH.NET <dev@vincenth.net>
2022-11-07 14:58:25 +0100

    Add copy of team A sln as team B sln

---
+++ src/Example/eShopByTwoTeams/TeamB/CatalogService/CatalogGrain.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+
+namespace Applicita.eShop.CatalogService;
+
+sealed class CatalogGrain : Grain, ICatalogGrain
+{
+    [GenerateSerializer]
+    internal sealed class Catalog
+    {
+        [Id(0)] public int NewProductId { get; set; } = 0;
+        [Id(1)] public List<Product> Products { get; set; } = new();
+    }
+
+    readonly IPersistentState<Catalog> catalog;
+
+    public CatalogGrain([PersistentState("state")] IPersistentState<Catalog> catalog) => this.catalog = catalog;
+
+    public async Task<int> CreateProduct(Product product)
+    {
+        int newProductId = catalog.State.NewProductId++;
+        catalog.State.Products.Add(product with { Id = newProductId });
+        await catalog.WriteStateAsync();
+        return newProductId;
+    }
+
+    public Task<ImmutableArray<Product>> GetAllProducts() => Task.FromResult(ImmutableArray.CreateRange(catalog.State.Products));
+
+    public Task<ImmutableArray<Product>> GetCurrentProducts(ImmutableArray<int> productIds)
+    {
+        var products = catalog.State.Products;
+        var currentProducts = productIds.Select(id => products.FirstOrDefault(p => p.Id == id)).OfType<Product>();
+        return Task.FromResult(currentProducts.ToImmutableArray());
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var products = catalog.State.Products;
+        for (int i = 0; i < products.Count; i++)
+        {
+            if (products[i].Id == product.Id)
+            {
+                products[i] = product;
+                await catalog.WriteStateAsync();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public async Task<bool> DeleteProduct(int id)
+    {
+        var products = catalog.State.Products;
+        for (int i = 0; i < products.Count; i++)
+        {
+            if (products[i].Id == id)
+            {
+                products.RemoveAt(i);
+                await catalog.WriteStateAsync();
+                return true;
+            }
+        }
+        return false;
+    }
+}
