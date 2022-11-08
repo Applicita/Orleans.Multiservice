9eee0c89249786c54df135b20db273aae278b285
VincentH.NET <dev@vincenth.net>
2022-11-08 10:58:16 +0100

    Change title of Swagger UI's in two team example to include team name

--- src/Example/eShopByTwoTeams/TeamB/Apis/Foundation/Program.cs
+++ src/Example/eShopByTwoTeams/TeamB/Apis/Foundation/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => {
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml"));
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Example eShop API by single team", Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Example eShop API by team B of 2", Version = "v1" });
 });
 
 var app = builder.Build();
