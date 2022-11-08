bb8deec4df6cb31fcf74ded0c283d1a864dcfc0c
VincentH.NET <dev@vincenth.net>
2022-11-08 10:54:43 +0100

    fix for GetCurrentProducts functionality is missing from catalog controller (in single team example and in two teams example)

--- src/Example/eShopBySingleTeam/TeamA/Apis/CatalogApi/CatalogController.cs
+++ src/Example/eShopBySingleTeam/TeamA/Apis/CatalogApi/CatalogController.cs
@@ -7,7 +7,7 @@ namespace Applicita.eShop.Apis.CatalogApi;
 [ApiController]
 public class CatalogController : ControllerBase
 {
-    const string Products  = "products";
+    const string Products = "products";
     const string Product = Products + "/{id}";
 
     readonly ICatalogGrain catalog;
@@ -21,11 +21,14 @@ public class CatalogController : ControllerBase
     public async Task<ActionResult<int>> CreateProduct(Product product)
         => CreatedAtAction(nameof(CreateProduct), await catalog.CreateProduct(product));
 
-    /// <response code="200">All products are returned</response>
+    /// <response code="200">
+    /// Products for all <paramref name="id"/>'s currently in the catalog are returned; unknown product id's are skipped.
+    /// If no <paramref name="id"/>'s are specified, all products in the catalog are returned
+    /// </response>
     [HttpGet(Products)]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<ImmutableArray<Product>> GetAllProducts()
-        => await catalog.GetAllProducts();
+    public async Task<ImmutableArray<Product>> GetProducts([FromQuery] int[]? id = null)
+        => await (id is null ? catalog.GetAllProducts() : catalog.GetCurrentProducts(id.ToImmutableArray()));
 
     /// <response code="200">The product is updated</response>
     /// <response code="404">The product id is not found</response>
